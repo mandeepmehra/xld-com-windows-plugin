d7fbad4ae88f15169fa317a1c888d8f4cb72712d
Mandeep Mehra <mmehra@xebialabs.com>
2018-02-08 23:36:10 +0530

    Added force copy option

--- src/main/resources/wincom/registerdll.bat
+++ src/main/resources/wincom/registerdll.bat
@@ -1,7 +1,7 @@
 regsvr32 -u -s ${deployed.targetDirectory}\${deployed.targetFileName}
 IF %ERRORLEVEL% EQU 4 echo "Error in unregistering DLL"
 
-copy ${step.uploadedArtifactPath} ${deployed.targetDirectory}
+copy /Y ${step.uploadedArtifactPath} ${deployed.targetDirectory}
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 
 regsvr32 -s ${deployed.targetDirectory}\${deployed.targetFileName}
