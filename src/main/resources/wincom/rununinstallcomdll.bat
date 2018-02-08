887f216b6a55d9a202fd0371b9748ca5debf68ae
Mandeep Mehra <mmehra@xebialabs.com>
2018-02-08 19:45:52 +0530

    Update rununinstallcomdll.bat

--- src/main/resources/wincom/rununinstallcomdll.bat
+++ src/main/resources/wincom/rununinstallcomdll.bat
@@ -1,1 +1,2 @@
-echo To be Implemented
+cscript managecomdll.vbs -app ${deployed.appName} -dll ${deployed.targetDirectory}\${deployed.targetFileName} -com ${deployed.componentName}
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
