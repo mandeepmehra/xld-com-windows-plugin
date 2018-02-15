2e06dfb2dc813e426f6a35504842f2654c2b321c
Mandeep Mehra <mmehra@xebialabs.com>
2018-02-15 10:14:29 +0400

    Update runinstallcomdll.bat

--- src/main/resources/wincom/runinstallcomdll.bat
+++ src/main/resources/wincom/runinstallcomdll.bat
@@ -1,16 +1,15 @@
-copy /Y ${step.uploadedArtifactPath} ${deployed.targetDirectory}
-IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
-
 cscript managecomdll.vbs -app ${deployed.appName} -dll ${deployed.targetFileName} -com ${deployed.componentName} -remove
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 
 regsvr32 -u -s ${deployed.targetDirectory}\${deployed.targetFileName}
-IF %ERRORLEVEL% EQU 4 echo "Error in unregistering DLL"
+IF %ERRORLEVEL% EQU 0 echo "Error in unregistering DLL"
+
+copy /Y ${step.uploadedArtifactPath} ${deployed.targetDirectory}
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 
 cscript managecomdll.vbs -app ${deployed.appName} -dll ${deployed.targetDirectory}\${deployed.targetFileName} -com ${deployed.componentName}
 IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
 
 
 regsvr32 -s ${deployed.targetDirectory}\${deployed.targetFileName}
-IF %ERRORLEVEL% EQU 4 exit /b 0
-IF %ERRORLEVEL% NEQ 4 exit /b %ERRORLEVEL%
+IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
